635de1e404db25d9245163c884cf10dc75816bdb
areej-png <areejamir890@gmail.com>
2025-11-01 07:20:21 -0700

    feat: add complete weather data display with icon

--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -72,8 +72,27 @@ function WeatherApp() {
 
       {weatherData && (
         <div className="weather-card">
-          <h2>{weatherData.name}</h2>
-          <p>Temperature: {weatherData.main?.temp}°C</p>
+          {/*Phase 4 step 1 city name & country */}
+          <h2>{weatherData.name}, {weatherData.sys?.country}</h2>
+
+          {/* {Weather icon} */}
+           <img 
+      src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
+      alt={weatherData.weather[0].description}
+    />
+
+    {/* {Temperature} */}
+          <p className='temperature'>{Math.round(weatherData.main?.temp)}°C</p>
+
+          {/* {Description} */}
+           <p className="description">{weatherData.weather[0].description}</p>
+
+           {/* {Additional details} */}
+           <div className="weather-details">
+      <p>Feels like: {Math.round(weatherData.main?.feels_like)}°C</p>
+      <p>Humidity: {weatherData.main?.humidity}%</p>
+      <p>Wind Speed: {weatherData.wind?.speed} m/s</p>
+    </div>
         </div>
       )}
     </div>
