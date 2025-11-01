bc5c988481f3227226e63352693b6d09784d9d14
areej-png <areejamir890@gmail.com>
2025-11-01 02:10:42 -0700

    feat: phase 2 complete - UIstructure with states

--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -6,13 +6,40 @@ function WeatherApp() {
   const [error, setError] = useState('');
 
   const API_KEY = import.meta.env.VITE_WEATHER_API_KEY; 
+  const handleSearch = () => {
+  }
+ return ( 
+  <div className="weather">
+     {/* Phase 2 step 1*/}
+    <input
+      type="text"
+      value={city}
+      onChange={(e) => setCity(e.target.value)}
+      placeholder="Enter city name"
+    />
+    <button onClick={handleSearch}>Search</button>
+    {/*Phase 2step 2 */}
+    {/*case 1 Weather Data checked */}
+    {!weatherData && !loading && !error && (
+      <p>Search for a city</p>
+    )}
 
- console.log(city);
- console.log(API_KEY);
-  return (
-    <div className="weather">
-      {/*UI structure*/}
-    </div>
-  );
+    {/* Case 2: Loading state */}
+    {loading && <p>Loading...</p>}
+
+    {/* Case 3: Error state */}
+    {error && <p style={{color: 'red'}}>{error}</p>}
+
+    {/* Case 4: Weather data display */}
+    {weatherData && (
+      <div className="weather-card">
+        <h2>{weatherData.name}</h2>
+        <p>Temperature: {weatherData.main?.temp}°C</p>
+      </div>
+    )}
+
+  </div>
+  
+); 
 }
 export default WeatherApp;
